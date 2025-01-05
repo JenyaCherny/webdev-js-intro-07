74145d2243faafeed8c8ecc57cbe93b7502a52dc
JenyaCherny <jenya.cherny@gmail.com>
2025-01-05 12:13:58 -0500

    completed functionality coding in assignments

--- js/assignment.js
+++ js/assignment.js
@@ -8,7 +8,30 @@ const lostNumbersElement = document.getElementById("lost-numbers");
 const lostNumbers = [4, 8, 15, 16, 23, 42];
 
 
-function returnTheRemainder(a, b) 
-{ return a % b; } 
-const result = returnTheRemainder(12, 5); 
+function returnTheRemainder(a, b) { 
+    return a % b; 
+} 
+const result = returnTheRemainder(5, 4); 
 console.log(result);
+
+function checkIfEven(number){
+   return number % 2 === 0; 
+}
+console.log(checkIfEven(20));
+console.log(checkIfEven(47));
+
+function getTheFourthElement(array) { 
+    return array[3]; 
+}
+const fourthElement = getTheFourthElement(lostNumbers);
+console.log(fourthElement);
+
+function render() {
+    returnTheRemainder(a, b);
+    checkIfEven(number);
+    getTheFourthElement(array);
+}
+
+submissionBtn.addEventListener("click", function () {
+    render();
+});
